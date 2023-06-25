644cf2bc223d3f5af88159315e489666ff8b87f6
Bryan Goodrich <goodrich.bryan@gmail.com>
2023-06-25 20:34:20 +0000

    Setup Fullstack lab and separate workspaces

---
+++ Fullstack/restaurant/views.py
@@ -0,0 +1,46 @@
+# from django.http import HttpResponse
+import json
+
+from datetime import datetime
+
+from django.shortcuts import render
+from django.core import serializers
+
+from .models import Booking, Menu
+from .forms import BookingForm
+
+
+def home(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def book(request):
+    form = BookingForm()
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form': form}
+    return render(request, 'book.html', context)
+
+def booking(request):
+    date = request.GET.get('date', datetime.today().date())
+    bookings = Booking.objects.all()
+    booking_json = serializers.serialize('json', bookings)
+    return render(request, 'booking.html', {'bookings': booking_json})
+
+
+def menu(request):
+    menu_data = Menu.objects.all()
+    main_data = {"menu": menu_data}
+    return render(request, 'menu.html', {"menu": main_data})
+
+
+def display_menu_item(request, pk=None): 
+    menu_item = Menu.objects.get(pk=pk) if pk else ""
+    return render(request, 'menu_item.html', {"menu_item": menu_item})
+
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # coursera-meta-backend-littlelemon
+
 A repo for my Coursera Meta Back-End Developer certification course work and codespace.
+
+## API
+
+The API folder contains the final project for the APIs course. It was a considerable challenge compared to everything else in this program, but in the end it can be seen as pretty simple once you fully appreciate all the components. Unfortuntely, I think they did not prepare us for all the content used in even their final result. 
+
+
+### The Full Stack
+
+The Fullstack folder contains the final project template we built from to design the Booking API.
+
